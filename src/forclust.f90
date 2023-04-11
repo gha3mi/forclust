16a44b725eee15539573575bb1407d2772c978cf
Ali <info@gha3mi.com>
2023-04-11 08:56:42 +0200

    Fixed #4. Updated "find_number_of_nodes" subroutine.

--- src/forclust.f90
+++ src/forclust.f90
@@ -90,7 +90,7 @@ module forclust
       type(linux_backlight)                         :: backlight
    contains
       procedure          :: select => select_linux
-      procedure, private :: find_number_of_nodes ! todo:
+      procedure, private :: find_number_of_nodes
       procedure          :: print_info            => print_all_cluster_info
 
       procedure          :: deselect              => deallocate_cluster
@@ -103,18 +103,20 @@ contains
 
    !===============================================================================
    !> author: Seyed Ali Ghasemi
-   elemental pure subroutine find_number_of_nodes(this)
+   elemental impure subroutine find_number_of_nodes(this)
+      use :: popen_module, only: get_command_as_string
       class(cluster), intent(inout) :: this
+      character(len=:), allocatable :: nnodes_char
 
-      ! todo: get total number of linux nodes
-      this%nnodes = 1
+      nnodes_char = get_command_as_string("lscpu | grep 'NUMA node(s):' | awk '{print $3}'")
+      read(nnodes_char,*) this%nnodes
    end subroutine find_number_of_nodes
    !===============================================================================
 
 
    !===============================================================================
    !> author: Seyed Ali Ghasemi
-   elemental pure subroutine select_linux(this)
+   elemental impure subroutine select_linux(this)
       class(cluster), intent(inout) :: this
       character(len=100)            :: current_node_path
       character(len=100)            :: backlight_path
